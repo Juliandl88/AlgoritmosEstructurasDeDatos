67bbdd0fc7030b694c3206cd3357d5b0574a727a
Julian <julian_delisio@hotmail.com>
2023-01-05 23:27:28 -0300

    ejercicios programación funcional

--- ProgramacionFuncional.js
+++ ProgramacionFuncional.js
@@ -198,6 +198,29 @@ function remove(bookName) {
 
 /* Prepare 27 cups of green tea and 13 cups of black tea and store them in tea4GreenTeamFCC and  */
 
+// The global variable
+const bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+// Change code below this line
+function add(bookName) {
+
+  bookList.push(bookName);
+  return bookList;
+  
+  // Change code above this line
+}
+
+// Change code below this line
+function remove(bookName) {
+  const book_index = bookList.indexOf(bookName);
+  if (book_index >= 0) {
+
+    bookList.splice(book_index, 1);
+    return bookList;
+
+    // Change code above this line
+    }
+}
 
 
 /* ------------------------------------------------------------- */
