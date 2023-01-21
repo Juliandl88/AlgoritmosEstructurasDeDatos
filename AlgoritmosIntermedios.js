12bc9efe1721fed4b6d1272cfdb1ef706f8d6490
Julian <julian_delisio@hotmail.com>
2023-01-20 23:16:52 -0300

    Ejercicios al 52

--- AlgoritmosIntermedios.js
+++ AlgoritmosIntermedios.js
@@ -333,6 +333,62 @@ sumPrimes(10);
 
 /* -------------------------------------------------------------- */
 
+/* Encuentre el múltiplo común más pequeño de los parámetros proporcionados que se puede 
+dividir uniformemente por ambos, así como por todos los números secuenciales 
+en el rango entre estos parámetros.
+
+El rango será una matriz de dos números que no necesariamente estarán en orden numérico.*/
+
+
+function smallestCommons(arr) {
+  // Setup
+  const [min, max] = arr.sort((a, b) => a - b);
+  const numberDivisors = max - min + 1;
+  // Largest possible value for SCM
+  let upperBound = 1;
+  for (let i = min; i <= max; i++) {
+    upperBound *= i;
+  }
+  // Test all multiples of 'max'
+  for (let multiple = max; multiple <= upperBound; multiple += max) {
+    // Check if every value in range divides 'multiple'
+    let divisorCount = 0;
+    for (let i = min; i <= max; i++) {
+      // Count divisors
+      if (multiple % i === 0) {
+        divisorCount += 1;
+      }
+    }
+    if (divisorCount === numberDivisors) {
+      return multiple;
+    }
+  }
+}
+
+smallestCommons([1, 5]);
+
+
+/* -------------------------------------------------------------- */
+
+/* Dada la matriz arr, itere y elimine cada elemento comenzando desde el primer elemento (el índice 0) 
+hasta que la función funcregrese truecuando el elemento iterado pase a través de él.*/
+
+
+
+/* -------------------------------------------------------------- */
+
+/* Utilice la palabra clave delete para eliminar las claves oranges.*/
+
+/* -------------------------------------------------------------- */
+
+/* Utilice la palabra clave delete para eliminar las claves oranges.*/
+
+/* -------------------------------------------------------------- */
+
+/* Utilice la palabra clave delete para eliminar las claves oranges.*/
+
+/* -------------------------------------------------------------- */
+
 /* Utilice la palabra clave delete para eliminar las claves oranges.*/
 
 /* -------------------------------------------------------------- */
