24ba2162111407a3c2b545c055b9d1a7e22757e8
Julian <julian_delisio@hotmail.com>
2023-01-23 12:17:04 -0300

    Estructura de datos

---
+++ ProyectoEstructuraDatos.js
@@ -0,0 +1,225 @@
+/* Return true if the given string is a palindrome. Otherwise, return false.
+
+A palindrome is a word or sentence that's spelled the same way both forward and backward, ignoring punctuation, case, and spacing.
+
+Note: You'll need to remove all non-alphanumeric characters (punctuation, spaces and symbols) and turn everything into the same case (lower or upper case) in order to check for palindromes.
+
+We'll pass strings with varying formats, such as racecar, RaceCar, and race CAR among others.
+
+We'll also pass strings with special symbols, such as 2A3*3a2, 2A3 3a2, and 2_A3*3#A2. */
+
+function palindrome(str) {
+  // remove non-alphanumeric characters and convert to lowercase
+  var cleaned = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  // check if cleaned string is equal to its reverse
+  return cleaned === cleaned.split("").reverse().join("");
+}
+
+palindrome("eye");
+
+/* -------------------------------------------------------------- */
+
+/* Convert the given number into a roman numeral.
+
+Roman numerals	Arabic numerals
+M	1000
+CM	900
+D	500
+CD	400
+C	100
+XC	90
+L	50
+XL	40
+X	10
+IX	9
+V	5
+IV	4
+I	1
+All roman numerals answers should be provided in upper-case. */
+
+function convertToRoman(num) {
+  let lookup = {
+      M: 1000,
+      CM: 900,
+      D: 500,
+      CD: 400,
+      C: 100,
+      XC: 90,
+      L: 50,
+      XL: 40,
+      X: 10,
+      IX: 9,
+      V: 5,
+      IV: 4,
+      I: 1,
+    },
+    roman = "",
+    i;
+  for (i in lookup) {
+    while (num >= lookup[i]) {
+      roman += i;
+      num -= lookup[i];
+    }
+  }
+  return roman;
+}
+
+convertToRoman(36);
+
+/* -------------------------------------------------------------- */
+
+/*  Caesars Cipher
+One of the simplest and most widely known ciphers is a Caesar cipher, also known as a shift cipher. In a shift cipher the meanings of the letters are shifted by some set amount.
+
+A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus A ↔ N, B ↔ O and so on.
+
+Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+
+All letters will be uppercase. Do not transform any non-alphabetic character (i.e. spaces, punctuation), but do pass them on. */
+
+function rot13(str) {
+  let input = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let output = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+  let index = (x) => input.indexOf(x);
+  let translate = (x) => (index(x) > -1 ? output[index(x)] : x);
+  return str.split("").map(translate).join("");
+}
+
+rot13("SERR PBQR PNZC");
+
+/* -------------------------------------------------------------- */
+
+/* Validador de número de teléfono
+Regresa truesi la cadena pasada parece un número de teléfono válido de EE. UU.
+
+El usuario puede completar el campo del formulario de la forma que elija, siempre que tenga el formato de un número de EE. UU. válido. Los siguientes son ejemplos de formatos válidos para números de EE. UU. (consulte las pruebas a continuación para conocer otras variantes):
+
+555-555-5555
+(555)555-5555
+(555) 555-5555
+555 555 5555
+5555555555
+1 555 555 5555
+Para este desafío, se le presentará una cadena como 800-692-7753o 8oo-six427676;laskdjf. 
+Su trabajo es validar o rechazar el número de teléfono de EE. UU. 
+en función de cualquier combinación de los formatos proporcionados anteriormente. 
+El código de área es requerido. Si se proporciona el código de país, debe confirmar 
+que el código de país es 1. Devuelve truesi la cadena es un número de teléfono válido de EE. UU.; 
+de lo contrario regresa false. */
+
+function telephoneCheck(str) {
+  let result =
+    /^([+]?1[\s]?)?((?:[(](?:[2-9]1[02-9]|[2-9][02-8][0-9])[)][\s]?)|(?:(?:[2-9]1[02-9]|[2-9][02-8][0-9])[\s.-]?)){1}([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2}[\s.-]?){1}([0-9]{4}){1}$/;
+  return result.test(str);
+}
+
+telephoneCheck("555-555-5555");
+
+/* -------------------------------------------------------------- */
+
+/* JavaScript Algorithms and Data Structures
+JavaScript Algorithms and Data Structures Projects
+Cash Register
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+Currency Unit	Amount
+Penny	$0.01 (PENNY)
+Nickel	$0.05 (NICKEL)
+Dime	$0.1 (DIME)
+Quarter	$0.25 (QUARTER)
+Dollar	$1 (ONE)
+Five Dollars	$5 (FIVE)
+Ten Dollars	$10 (TEN)
+Twenty Dollars	$20 (TWENTY)
+One-hundred Dollars	$100 (ONE HUNDRED)
+See below for an example of a cash-in-drawer array:
+
+[
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+]
+*/
+
+function checkCashRegister(price, cash, cid) {
+  let denom = [
+    { name: "ONE HUNDRED", val: 100 },
+    { name: "TWENTY", val: 20 },
+    { name: "TEN", val: 10 },
+    { name: "FIVE", val: 5 },
+    { name: "ONE", val: 1 },
+    { name: "QUARTER", val: 0.25 },
+    { name: "DIME", val: 0.1 },
+    { name: "NICKEL", val: 0.05 },
+    { name: "PENNY", val: 0.01 },
+  ];
+  let output = { status: null, change: [] };
+  let change = cash - price;
+  let register = cid.reduce(
+    function (acc, curr) {
+      acc.total += curr[1];
+      acc[curr[0]] = curr[1];
+      return acc;
+    },
+    { total: 0 }
+  );
+  if (register.total === change) {
+    output.status = "CLOSED";
+    output.change = cid;
+    return output;
+  }
+  if (register.total < change) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+  let change_arr = denom.reduce(function (acc, curr) {
+    let value = 0;
+    while (register[curr.name] > 0 && change >= curr.val) {
+      change -= curr.val;
+      register[curr.name] -= curr.val;
+      value += curr.val;
+      change = Math.round(change * 100) / 100;
+    }
+    if (value > 0) {
+      acc.push([curr.name, value]);
+    }
+    return acc;
+  }, []);
+  if (change_arr.length < 1 || change > 0) {
+    output.status = "INSUFFICIENT_FUNDS";
+    return output;
+  }
+  output.status = "OPEN";
+  output.change = change_arr;
+  return output;
+}
+
+checkCashRegister(19.5, 20, [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+]);
+
+/* -------------------------------------------------------------- */
